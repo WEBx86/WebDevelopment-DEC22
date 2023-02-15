546e62f18e53070e8f1a1dbe9984ecdaf8d86d83
WEBx86 <webx86infotech@gmail.com>
2023-02-15 23:17:22 +0530

    Update Control Flow Statements

--- JavaScript/Statements.js
+++ JavaScript/Statements.js
@@ -60,15 +60,148 @@
 // 0   1   2  3  4
 // "1  2   3  4  5"
 
-var x = "12345";
-var temp = "";
-// Output - 54321
-console.log(x.length); // 5
+// var x = "12345";
+// var temp = "";
+// // Output - 54321
+// console.log(x.length); // 5
 
-for (let i = x.length - 1; i >= 0; i--)
+// for (let i = x.length - 1; i >= 0; i--)
+// {
+//     temp = temp + x[i];
+// }
+
+// console.log(temp);
+
+
+
+//var num = [10, 35, 68, 91, 25];
+
+// for (let i = 0; i <= num.length - 1; i++)
+// {
+//     if (num[i] % 2 == 0)
+//     {
+//         console.log(`Given Number is Even = ${num[i]}`);
+//     }
+//     else
+//     {
+//         console.log(`Given Number is Odd = ${num[i]}`);
+//     }
+// }
+
+// for (let i = 1; i <= 5; i++)
+// {
+//     console.log("Welcome");
+// }
+
+// var i = 0
+
+// while (i < 5)
+// {
+//     console.log("Welcome");
+//     i++;
+// }
+
+// let x = 0;
+// let y = 0;
+  
+// while (x < 3)
+// {
+//     x++;   // 1, 2, 3
+//     y = y + x; // 1      y = 1 + 2  //y = 3 + 3 = 6
+// }
+
+// console.log(y);
+var StoreValue = '';
+for (let i = 0; i < 10; i++){
+
+    if (i == 4)
+    {
+        continue;
+    }
+    else
+    {
+        StoreValue += i;
+    }    
+}
+// console.log(StoreValue);
+// var i = 0;
+// var j = 0;
+
+// while (i < 5)
+// {                      // i = 0 1 2 3
+//     if (i === 3)
+//     {
+//         continue;
+//     }
+//     i++;
+//     j = j + i;        // j = 1 + 2 = 3 + 3 = 6
+// }
+
+// console.log(j);
+
+// let Day = "Test 01";
+
+// if (Day == "Monday")
+// {
+//     console.log("Today is Monday");
+// }
+// else if (Day == "Tuesday")
+// {
+//     console.log("Today is Tuesday");
+// }
+// else if (Day == "Wednesday")
+// {
+//     console.log("Today is Wednesday");
+// }
+// else if (Day == "Thursday")
+// {
+//     console.log("Today is Thursday");
+// }
+// else if (Day == "Friday")
+// {
+//     console.log("Today is Friday");
+// }
+// else if (Day == "Saturday")
+// {
+//     console.log("Today is Saturday");
+// }
+// else if (Day == "Sunday")
+// {
+//     console.log("Today is Sunday");
+// }
+// else
+// {
+//     console.log("Please Enter Correct Day"); 
+// }
+
+var Day = "Wednesday";
+
+switch (Day)
 {
-    temp = temp + x[i];
+    default:
+        console.log("Enter valid day");
+        break;
+    case "Monday":
+        console.log("Monday");
+        break;
+    case "Tuesday":   
+    case "Wednesday":
+        console.log("Wednesday");
+        break;
+    case "Thursday":
+        console.log("Thursday");
+        break;
+    case "Friday":
+        console.log("Friday");
+        break;
+    case "Saturday":
+        console.log("Saturday");
+        break;
+    case "Sunday":
+        console.log("Sunday");
+        break;
 }
 
-console.log(temp);
+
+
 
