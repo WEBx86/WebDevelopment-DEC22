7f3fd74241f87eff16716f05b0066ee299e5fa3b
WEBx86 <webx86infotech@gmail.com>
2023-02-15 12:18:17 +0530

    Javascript Datatypes

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,134 @@
+
+/* 
+1. number
+      101, 1, 152.564, 135.5, 0.2565, 45555e.4455
+2. string 
+      "Web", 'DemoData', '', "","10",'564.255'    
+3. Bigint   
+        var x = Bigint("5445544445555555545555555");
+        
+4. boolean
+     true    
+     false
+     
+5. undefined 
+6. null      
+*/
+
+
+// var x = 10.104;
+// console.log(typeof x);
+
+// var Name = 'Hello Javascript' + 'I am web Developer';
+// console.log(Name); // Hello JavascriptI am web Developer
+// console.log(typeof (Name));
+
+// var MyDemo = 10 + "20";  // "10" + "20" // 1020
+// console.log(MyDemo);
+// console.log(typeof MyDemo);
+
+// var MyDemo = "20" + 30;  // "20" + "30"  // 2030
+// console.log(MyDemo);
+// console.log(typeof MyDemo);
+
+// MyDemo = 9 - "8";
+// console.log(MyDemo);
+// console.log(typeof MyDemo);
+
+// MyDemo = "Hello" - "World";
+// console.log(MyDemo);
+// console.log(typeof MyDemo);
+          
+
+// console.log(10 + 20 + 5 + "Hello Javascript" + 10 + 30 + 40 );
+
+// var x = undefined;
+// console.log(typeof (x));
+// var x = null;
+// console.log(x);
+// const x = [10, 20, 30];
+// console.log(typeof x);
+
+
+// const x = { Name: "x", age: 20, RollNo: 101 };
+// console.log(typeof (x));
+
+// Datatypes
+/*
+ Primary Datatypes / Primitive
+
+ 1. number 
+       e.g - 1, 10.56, -1, infinity 
+ 2. string 
+       e.g - 'WEB', "1001", '10.25', 'true', 'A', ""
+ 3. boolean 
+      e.g - true, false 
+ 4. BigInt
+      e.g - var x = BigInt('85644455665445522556');
+ 5. undefined 
+      e.g - var x;
+            - undefined
+ 6. Null 
+      e.g - var x = null; 
+             typeof(x);
+
+ Non Primary / Object Typed 
+
+  1. Array - []
+  2. Object - {}
+
+  typeof()
+
+  Array Index start from 0
+
+*/
+
+// var First = "Test 01";
+// var Second = "Test 02";
+// var Third = "Test 03";
+
+// console.log(First);
+// console.log(Second);
+// console.log(Third);
+
+// var MyArr = ["Web Development", true, 101, 'Hello JS'];
+
+// var MyArr02 = ["Web", "Development", "Hello", "Java Script"];
+
+// console.log(MyArr);
+// console.log(MyArr02)
+
+// console.log(typeof (MyArr));
+// console.log(typeof (MyArr02));
+
+// // Array Literal
+// // const Arr_Name = [element01, element02, element03,.....];
+
+// // const Arr_Name = new Array(element01, element02, element03,...);
+// const Arr_Name = [10];
+// console.log(Arr_Name);
+
+// const Arr_Name02 = new Array(10);
+// console.log(Arr_Name02);
+// var MyArr = ["Web Development", true, 101, 'Hello JS'];
+// console.log(MyArr.length);
+
+// const NumArr = [10, 20, 30, 40, 50];
+// const x = NumArr.length;
+// console.log(x);
+
+// const StudentMarks01 = [70, 80, 60, 90, 69];
+// StudentMarks01[StudentMarks01.length] = 100;
+// const LastIndex = StudentMarks01.length - 1;
+// console.log(StudentMarks01[LastIndex]);
+
+// StudentMarks01[3] = "Hello";
+// console.log(StudentMarks01);
+
+// Object
+const StudentInformation = { Name: "TestName", Address: "ABCD", Age: 20, Grade: true };
+console.log(StudentInformation.Name, StudentInformation.Address);
+
+// JavaScript Object Notation = JSON
+
+
