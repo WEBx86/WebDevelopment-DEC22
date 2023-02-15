dae7b8df8479e11d3405a787d1a65634236a93b3
WEBx86 <webx86infotech@gmail.com>
2023-02-15 12:20:47 +0530

    JavaScript Operators

---
+++ JavaScript/Operators.js
@@ -0,0 +1,62 @@
+/*
+   1. Arithmatic Operator 
+           -, +, *, %, /, **, ++, --
+           10 ** 2 = 100;
+           Math.pow(10,2);
+
+    2. Comparison Operator 
+         ==, !=, ===,!== , >, <, >=, <=, ?
+
+    3. Assignment Operator
+          =, +=, -=, /=, *= ,**=
+    4. Logical Operator 
+          &&, || , !
+    5. Bitwise Operator
+          &,|, ^, ~, >>, <<, >>> 
+*/
+
+// var x = 10;
+// var y = 3;
+// console.log(x ** y);
+// console.log(Math.pow(10, 3));
+
+// // Increment Operator/////////
+// // Post Increment
+// var x = 10;
+// console.log(x++);// 10
+// var y = x++;
+// console.log(y); // 11
+
+// //Pre Increment
+// let x = 20;
+// console.log(++x); // 21
+// let y = ++x;
+// console.log(y);  // 22
+
+// //Decrement Operator ///////////
+// //Post Decrement
+// let x = 10;
+// let y = x--;   // y = 10, x= 9
+// console.log(x); // 9
+// console.log(y); // 10
+
+// //pre Decrement
+// let a = 10;
+// let b = --a; // b = 9, a = 9
+
+// console.log(a); //9
+// console.log(b); //9
+
+// ===  Comparison Operator
+// !== Strictly Not Equal
+// Strict Equality
+
+console.log(1 !== 1); // False 
+console.log("1" !== 1);
+console.log(0 !== false);
+console.log("true" !== 1);
+
+
+
+
+
