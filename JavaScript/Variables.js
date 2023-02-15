9e60034dede4a1bcb279eb556886f26307567ecc
WEBx86 <webx86infotech@gmail.com>
2023-02-15 12:26:27 +0530

    Variables In Javascript

---
+++ JavaScript/Variables.js
@@ -0,0 +1,164 @@
+/*
+    1. Must start with aplphabets(A-Z Or a-z), underscore(_) , doller sign ($)
+    2. cannot be start with number 
+    3. do not use space between variable name
+    4. Reserved words or keyword cannot be used as variable names.
+
+    var
+    let 
+    const 
+    if 
+    else
+    while 
+    do while 
+    break
+    for
+
+    var x = 10;
+    var X = 20;
+
+    10 - 
+    3.14 - 
+    "hello" -  string 
+    true    - boolean
+
+    int x = 10;
+    float pi = 3.14;
+    string Name = "Hello";
+    10 
+    int x = 10;
+*/
+
+/*
+   Hoisting
+
+   var let const
+
+   integer
+   string 
+   boolean
+   floating 
+
+   array 
+   object 
+
+   10
+
+   int x = 10;
+   char name[50] = "WEB";
+   bool x = true;
+
+
+   string Name = "";
+
+   
+*/
+
+//fuctional scope
+
+/*
+   1. Global Scope 
+   2. Function Scope 
+*/
+
+// x = 10;
+// console.log(typeof(x),x);
+
+// x = "Web Development";
+// console.log(typeof(x),x);
+
+// x = true;
+// console.log(typeof(x),x);
+
+// x = 125.50;
+// console.log(typeof(x),x);
+
+// x = "Hello World";
+// console.log(typeof(x),x);
+
+// {
+//     var x = 10;
+// }
+
+// console.log("Assigned Value : ",x);
+/*
+var x = "Web Development";
+
+function MyValue(){
+    var x = 10;
+    console.log(x);
+
+    if(true)
+    {
+        x = 20; 
+        console.log(x);
+    }
+    console.log(x);
+}
+
+MyValue();
+console.log(x);
+*/
+
+
+
+// let & const => scope - Block Scope
+
+
+// function Hello(){
+//     let x = 20; // global
+//     console.log(x);
+
+//     if(true)
+//     {
+//         let y = "Hello World";
+//         console.log(y);
+//     }
+
+//     console.log(y);
+// }
+// Hello();
+
+// let x = "Hello";
+// console.log(x);
+
+// {
+//     x = 10;
+//     console.log(x);
+
+//     let y = "Varibale y is decleared";
+//     console.log(y);
+// }
+
+// console.log(x);
+// console.log(y);
+
+// const No = 10;
+// No = 20;
+
+// const a = 10;
+// const b = 20;
+
+// const c = 10 + 20;
+// console.log(c);
+
+// const arr = [10,20,30,40];
+// arr[0] = 100;
+// console.log(arr);
+ 
+// const obj = {name:"myName",age:20,Std:"1st"};
+// obj.name = "Hello My bject";
+// console.log(obj);
+ 
+
+  
+
+
+
+
+
+
+
+
+
+
