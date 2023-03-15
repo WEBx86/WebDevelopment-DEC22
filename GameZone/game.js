6f3b888df61b66823e82bb383a3ecd47ae3265c3
WEBx86 <webx86infotech@gmail.com>
2023-03-15 23:09:42 +0530

    Your Car Game Using Javascript

--- GameZone/game.js
+++ GameZone/game.js
@@ -27,6 +27,20 @@ function keyUp(e){
     console.log(keys);
 }
 
+function playLines() {
+    let lines = document.querySelectorAll('.lines');
+
+    lines.forEach(function (element) {
+
+        if (element.y >= 850) {
+            element.y -= 900;
+        }
+
+        element.y += You.carSpeed;
+        element.style.top = element.y + "px";
+    })
+}
+
 //Part VI
 document.addEventListener('click',start);
 
@@ -38,11 +52,13 @@ function playYourGame(){
     let Car = document.querySelector('.Car');
 
     let YoueGameRoad = gameArea.getBoundingClientRect();
-    console.log(YoueGameRoad);
+   // console.log(YoueGameRoad);
     
     if (You.start) {
+
+        playLines();
         
-        if (keys.ArrowUp && You.y > 0) {
+        if (keys.ArrowUp && You.y > (YoueGameRoad.top + 150)) {
             You.y -= You.carSpeed;
         }
 
@@ -57,10 +73,8 @@ function playYourGame(){
         if (keys.ArrowRight && You.x < YoueGameRoad.width - 100) {
             You.x += You.carSpeed;
         }
-
         Car.style.top = You.y + "px";    // Top Position 
         Car.style.left = You.x + "px";   // Left Position 
-
         window.requestAnimationFrame(playYourGame);
     }
 }
@@ -72,6 +86,15 @@ function start(){
     You.start = true;
     window.requestAnimationFrame(playYourGame);
 
+    // Create Your Road Lines
+    for (let i = 0; i < 8; i++){
+        let roadLine = document.createElement('div');
+        roadLine.setAttribute('class', 'lines');
+        roadLine.y = (i * 150);
+        roadLine.style.top = roadLine.y + "px";
+        gameArea.appendChild(roadLine);
+    }
+   
     // Create Your Car 
     let yourCar = document.createElement('div');
     yourCar.setAttribute('class','Car');
@@ -81,8 +104,8 @@ function start(){
     You.x = yourCar.offsetLeft;
     You.y = yourCar.offsetTop;
 
-    console.log("Left Side : ", yourCar.offsetLeft);
-    console.log("Top Side : ", yourCar.offsetTop);
+    // console.log("Left Side : ", yourCar.offsetLeft);
+    // console.log("Top Side : ", yourCar.offsetTop);
 }
 
 
