a667f155c6d97e22ecf97dc3b575299ec952933e
WEBx86 <webx86infotech@gmail.com>
2023-03-16 23:58:26 +0530

    Your Car Game Using Javascript

--- GameZone/game.js
+++ GameZone/game.js
@@ -1,15 +1,10 @@
 
-// Part III 
 const score = document.querySelector('.score');
 const startScreen = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
 
-
-// Part V
-
 let keys = { ArrowUp : false, ArrowDown : false, ArrowLeft : false, ArrowRight : false };
 
-// part IV
 document.addEventListener('keydown',keyDown);
 document.addEventListener('keyup',keyUp);
 
@@ -27,6 +22,17 @@ function keyUp(e){
     console.log(keys);
 }
 
+// para02 means other cars
+// para01 means your car
+
+function GameOver(para01,para02) {
+    YourCarPostion = para01.getBoundingClientRect();
+    OtherCarPosition = para02.getBoundingClientRect();  
+
+
+    return !((YourCarPostion.bottom < OtherCarPosition.top) || (YourCarPostion.top > OtherCarPosition.bottom) || (YourCarPostion.right < OtherCarPosition.left) || (YourCarPostion.left > OtherCarPosition.right))
+}
+
 function playLines() {
     let lines = document.querySelectorAll('.lines');
 
@@ -41,10 +47,35 @@ function playLines() {
     })
 }
 
-//Part VI
+function endYourGame() {
+    You.start = false;
+    startScreen.classList.remove('hide');
+    startScreen.innerHTML = "Game Over <br> Your Final Score is " + You.YourScore + "<br>Click here to restart your game";
+}
+
+function moveOtherCars(Car) {
+    let other = document.querySelectorAll('.other');
+
+    other.forEach(function (element) {
+
+        if (GameOver(Car, element)) {
+            console.log('Game Over');
+            endYourGame();
+        }
+
+        if (element.y >= 850) {
+            //element.y -= 900;
+            element.y = -400;
+            element.style.left = Math.floor(Math.random() * 350) + "px";
+        }
+        element.y += You.carSpeed;
+        element.style.top = element.y + "px";
+    })
+}
+
 document.addEventListener('click',start);
 
-var You = { carSpeed: 5};
+var You = { carSpeed: 5, YourScore: 0}; // 01
 
 function playYourGame(){
     console.log('Play The game');
@@ -57,12 +88,13 @@ function playYourGame(){
     if (You.start) {
 
         playLines();
+        moveOtherCars(Car);
         
-        if (keys.ArrowUp && You.y > (YoueGameRoad.top + 150)) {
+        if (keys.ArrowUp && You.y > (YoueGameRoad.top + 70)) {
             You.y -= You.carSpeed;
         }
 
-        if (keys.ArrowDown && You.y < YoueGameRoad.bottom - 150) {
+        if (keys.ArrowDown && You.y < YoueGameRoad.bottom - 70) {
             You.y += You.carSpeed;
         }
 
@@ -70,20 +102,27 @@ function playYourGame(){
             You.x -= You.carSpeed;
         }
 
-        if (keys.ArrowRight && You.x < YoueGameRoad.width - 100) {
+        if (keys.ArrowRight && You.x < YoueGameRoad.width - 50) {
             You.x += You.carSpeed;
         }
         Car.style.top = You.y + "px";    // Top Position 
         Car.style.left = You.x + "px";   // Left Position 
         window.requestAnimationFrame(playYourGame);
+        console.log(You.YourScore++);
+
+        You.YourScore++;
+        let FinalScore = You.YourScore - 2;
+        score.innerText = "Your game score is : " +  FinalScore;
     }
 }
 
 function start(){
-    gameArea.classList.remove('hide');
+   // gameArea.classList.remove('hide');
     startScreen.classList.add('hide');
+    gameArea.innerHTML = "";
 
     You.start = true;
+    You.YourScore = 0;  // 02
     window.requestAnimationFrame(playYourGame);
 
     // Create Your Road Lines
@@ -106,6 +145,17 @@ function start(){
 
     // console.log("Left Side : ", yourCar.offsetLeft);
     // console.log("Top Side : ", yourCar.offsetTop);
+
+    for (let j = 0; j < 4; j++){
+        let otherCar = document.createElement('div');
+        otherCar.setAttribute('class', 'other');
+        //otherCar.y = (j * 150);
+        otherCar.y = ((j + 1) * 350) * -1;
+        otherCar.style.top = otherCar.y + "px";
+        otherCar.style.background = "orange";
+        otherCar.style.left = Math.floor(Math.random() * 500) + "px";
+        gameArea.appendChild(otherCar);
+    }
 }
 
 
